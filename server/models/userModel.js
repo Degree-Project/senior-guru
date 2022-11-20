30f02918e8779f43822e9eccb1dec5827e940d95
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-11-20 12:32:08 +0530

    feat: user-route & utils

---
+++ server/models/userModel.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please Enter Your First Name"],
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please Enter Your Last Name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        unique: true,
+    },
+    gender: {
+        type: String,
+        required: [true, "Please Enter Gender"]
+    },
+    birthDate: {
+        type: Date,
+        default: Date.now,
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your Password"],
+        minLength: [8, "Password should have atleast 8 chars"],
+        select: false,
+    },
+    location: {
+        type: String,
+        required: [true, "Please Enter Your Location"]
+    },
+    avatar: {
+        public_id: {
+            type: String,
+        },
+        url: {
+            type: String,
+        }
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+}
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
