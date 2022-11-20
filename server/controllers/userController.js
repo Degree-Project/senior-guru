241eb2d5758d0156703fa65d4988a03855dde7d8
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-11-20 12:32:08 +0530

    feat: user-route & utils

---
+++ server/controllers/userController.js
@@ -0,0 +1,145 @@
+const User = require('../models/userModel');
+const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
+const sendToken = require('../utils/sendToken');
+const ErrorHandler = require('../utils/errorHandler');
+const cloudinary = require('cloudinary');
+
+// Register User
+exports.registerUser = asyncErrorHandler(async (req, res, next) => {
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 150,
+        crop: "scale",
+    });
+
+    const { firstName, lastName, email, gender, birthDate, location, password, role } = req.body;
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        gender,
+        birthDate,
+        location,
+        password,
+        role,
+        avatar: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+        },
+    });
+
+    sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = asyncErrorHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return next(new ErrorHandler("Please Enter Email And Password", 400));
+    }
+
+    const user = await User.findOne({ email}).select("+password");
+
+    if(!user) {
+        return next(new ErrorHandler("Invalid Email or Password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid Email or Password", 401));
+    }
+
+    sendToken(user, 201, res);
+});
+
+// Logout User
+exports.logoutUser = asyncErrorHandler(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    });
+});
+
+// Get User Details
+exports.getUserDetails = asyncErrorHandler(async (req, res, next) => {
+    
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+// Update User Profile
+exports.updateProfile = asyncErrorHandler(async (req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    }
+
+    if(req.body.avatar !== "") {
+        const user = await User.findById(req.user.id);
+
+        const imageId = user.avatar.public_id;
+
+        await cloudinary.v2.uploader.destroy(imageId);
+
+        const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+            folder: "avatars",
+            width: 150,
+            crop: "scale",
+        });
+
+        newUserData.avatar = {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+        }
+    }
+
+    await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: true,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+// Get All Users 
+exports.getAllUsers = asyncErrorHandler(async (req, res, next) => {
+
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users,
+    });
+});
+
+// Get Single User Details 
+exports.getSingleUser = asyncErrorHandler(async (req, res, next) => {
+
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return next(new ErrorHandler(`User doesn't exist with id: ${req.params.id}`, 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
---
+++ server/routes/userRoute.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { registerUser, loginUser, logoutUser, getUserDetails, updateProfile, getAllUsers, getSingleUser } = require('../controllers/userController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.route('/test').get((req, res) => {
+    res.send("I'm Working user route!!");
+  })
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').get(logoutUser);
+
+// router.route('/me').get(isAuthenticatedUser, getUserDetails);
+// router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+// router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("user", "admin"), getAllUsers);
+// router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("user", "admin"), getSingleUser);
+
+router.route('/me').get(getUserDetails);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+router.route("/users").get(getAllUsers);
+router.route("/user/:id").get(getSingleUser);
+
+
+module.exports = router;
