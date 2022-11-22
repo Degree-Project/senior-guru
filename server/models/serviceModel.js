da16cc741d0954b25ad7728288d0a4899d4561d5
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-11-22 15:39:58 +0530

    feat: service-model1

---
+++ server/models/serviceModel.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter service name"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter service description"]
+    },
+    highlights: [
+        {
+            type: String,
+        }
+    ],
+    specifications: [
+        {
+            title: {
+                type: String,
+            },
+            description: {
+                type: String,
+            }
+        }
+    ],
+    location: {
+        type: String,
+        required: [true, "Please enter service location"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please enter service price"]
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: [
+        {
+            type: String,
+            required: [true, "Please enter service category"]
+        }
+    ],
+    availableSlots: {
+        type: Number,
+        required: [true, "Please enter service slots"],
+        maxlength: [4, "slots cannot exceed limit"],
+        default: 1
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: "User",
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
