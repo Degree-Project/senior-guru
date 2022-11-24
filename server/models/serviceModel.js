8348cb43c2939230470b8116b3f3df0e08082423
Robin-Kumar-Singh <robin.singh@noustek.com>
2022-11-24 21:58:55 +0530

    Added fetch services by Location

--- server/models/serviceModel.js
+++ server/models/serviceModel.js
@@ -1,98 +1,106 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const serviceSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: [true, "Please enter service name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter service description"],
+  },
+  highlights: [
+    {
+      type: String,
+    },
+  ],
+  specifications: [
+    {
+      title: {
         type: String,
-        required: [true, "Please enter service name"],
-        trim: true
-    },
-    description: {
+      },
+      description: {
         type: String,
-        required: [true, "Please enter service description"]
+      },
     },
-    highlights: [
-        {
-            type: String,
-        }
-    ],
-    specifications: [
-        {
-            title: {
-                type: String,
-            },
-            description: {
-                type: String,
-            }
-        }
-    ],
-    location: {
+  ],
+  city: {
+    type: String,
+    required: [true, "Please enter your city name"],
+  },
+  location: {
+    type: {
+      type: String,
+    },
+    coordinates: [],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter service cost"],
+  },
+  images: [
+    {
+      public_id: {
         type: String,
-        required: [true, "Please enter service location"]
+      },
+      url: {
+        type: String,
+      },
     },
-    price: {
-        type: Number,
-        required: [true, "Please enter service cost"]
+  ],
+  category: [
+    {
+      type: String,
+      required: [true, "Please enter service category"],
     },
-    images: [
-        {
-            public_id: {
-                type: String
-            },
-            url: {
-                type: String
-            }
-        }
-    ],
-    category: [
-        {
-            type: String,
-            required: [true, "Please enter service category"]
-        }
-    ],
-    availableSlots: {
-        type: Number,
-        required: [true, "Please enter service slots"],
-        maxlength: [4, "slots cannot exceed limit"],
-        default: 1
-    },
-    ratings: {
-        type: Number,
-        default: 0
-    },
-    numOfReviews: {
-        type: Number,
-        default: 0
-    },
-    reviews: [
-        {
-            user: {
-                type: mongoose.Schema.ObjectId,
-                ref: "User",
-                required: true
-            },
-            name: {
-                type: String,
-                required: true
-            },
-            rating: {
-                type: Number,
-                required: true
-            },
-            comment: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    user: {
+  ],
+  availableSlots: {
+    type: Number,
+    required: [true, "Please enter service slots"],
+    maxlength: [4, "slots cannot exceed limit"],
+    default: 1,
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
-        required: true
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Service', serviceSchema);
+serviceSchema.index({ location: "2dsphere" });
+
+module.exports = mongoose.model("Service", serviceSchema);
--- server/routes/serviceRoute.js
+++ server/routes/serviceRoute.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const { getAllServices, getServices, getServiceDetails, createService, deleteService } = require('../controllers/serviceController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const express = require("express");
+const {
+  getAllServices,
+  getServices,
+  getServiceDetails,
+  createService,
+  deleteService,
+} = require("../controllers/serviceController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route('/test-s').get((req, res) => {
-    res.send("I'm Working service route!!");
-  })
+router.route("/test-s").get((req, res) => {
+  res.send("I'm Working service route!!");
+});
 
 // router.route('/services').get(getAllServices);
-router.route('/services/all').get(getServices);
 
-router.route('/admin/service/new').post(isAuthenticatedUser, createService);
+router.route("/services/all").post(getServices);
 
-router.route('/service/:id').get(getServiceDetails);
+router.route("/admin/service/new").post(isAuthenticatedUser, createService);
+
+router.route("/service/:id").get(getServiceDetails);
 
 module.exports = router;
