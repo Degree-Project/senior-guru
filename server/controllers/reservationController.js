1a0f8af0636febdb88948b02e463340fe5df156a
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-11-27 12:04:20 +0530

    feat: reservation-routes

---
+++ server/controllers/reservationController.js
@@ -0,0 +1,100 @@
+const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
+const ErrorHandler = require('../utils/errorHandler');
+const Service = require('../models/serviceModel');
+const Reservation = require('../models/reservationModel');
+
+exports.newReservation = asyncErrorHandler(async (req, res, next) => {
+
+    const {
+        reservationItem,
+        paymentStatus,
+        totalCost,
+    } = req.body;
+
+    // const reservationExist = await Reservation.findOne({ user: req.user._id });
+
+    // if (reservationExist) {
+    //     return next(new ErrorHandler("Reservation Already Placed", 400));
+    // }
+
+    const reservation = await Reservation.create({
+        reservationItem,
+        paymentStatus,
+        totalCost,
+        user: req.user._id,
+        contact: req.user.email
+    });
+
+    res.status(201).json({
+        success: true,
+        reservation,
+    });
+});
+
+// Get Single Reservation Details
+exports.getSingleReservationDetails = asyncErrorHandler(async (req, res, next) => {
+
+    const reservation = await Reservation.findById(req.params.id).populate("user", "firstName lastName email avatar");
+
+    if (!reservation) {
+        return next(new ErrorHandler("Reservation Not Found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        reservation,
+    });
+});
+
+
+// Get Logged In User Reservations
+exports.myReservations = asyncErrorHandler(async (req, res, next) => {
+
+    const reservations = await Reservation.find({ user: req.user._id });
+
+    if (!reservations) {
+        return next(new ErrorHandler("Reservation Not Found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        reservations,
+    });
+});
+
+// Get Logged In Guru Reservations
+exports.myGuruReservations = asyncErrorHandler(async (req, res, next) => {
+
+    const reservations = await Reservation.find({ 'reservationItem.guru': req.user._id });
+
+    if (!reservations) {
+        return next(new ErrorHandler("Reservation Not Found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        reservations,
+    });
+});
+
+async function updateSlots(id, quantity) {
+    const service = await Service.findById(id);
+    service.availableSlots -= quantity;
+    await service.save({ validateBeforeSave: false });
+}
+
+// Delete Reservation
+exports.deleteReservation = asyncErrorHandler(async (req, res, next) => {
+
+    const reservation = await Reservation.findById(req.params.id);
+
+    if (!reservation) {
+        return next(new ErrorHandler("Reservation Not Found", 404));
+    }
+
+    await reservation.remove();
+
+    res.status(200).json({
+        success: true,
+    });
+});
---
+++ server/routes/reservationRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const { newReservation, getSingleReservationDetails, myReservations, myGuruReservations, deleteReservation} = require('../controllers/reservationController');
+
+const router = express.Router();
+
+router.route("/test-r").get((req, res) => {
+    res.send("I'm Working reservation route!!");
+  });
+
+router.route('/reservation/new').post(isAuthenticatedUser, newReservation);
+router.route('/reservation/:id').get(isAuthenticatedUser, getSingleReservationDetails);
+router.route('/reservations/me').get(isAuthenticatedUser, myReservations);
+router.route('/guru/reservations/me').get(isAuthenticatedUser, myGuruReservations);
+
+router.route('/guru/reservation/:id').delete(isAuthenticatedUser, deleteReservation);
+
+module.exports = router;
