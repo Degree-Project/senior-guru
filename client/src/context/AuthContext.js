ce92e2f357e350455c03370b583ccc04e7608ad6
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2022-11-29 08:54:43 +0530

    Revert "Livio/cert"

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,33 +1,23 @@
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
-import { useGeolocated } from "react-geolocated";
 
 const AuthContext = createContext();
 
 function AuthContextProvider(props) {
   const [isAuthenticated, setIsAuthenticated] = useState();
-  const [locData, setLocData] = useState();
 
   const getLoggedIn = () => {
     axios.get("/api/loginStatus").then((res) => {
       setIsAuthenticated(res.data);
     });
   };
-  // console.log(data);
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-    useGeolocated({
-      positionOptions: {
-        enableHighAccuracy: false,
-      },
-      userDecisionTimeout: 5000,
-    });
+
   useEffect(() => {
     getLoggedIn();
-    setLocData(coords);
   });
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, getLoggedIn, locData }}>
+    <AuthContext.Provider value={{ isAuthenticated, getLoggedIn }}>
       {props.children}
     </AuthContext.Provider>
   );
